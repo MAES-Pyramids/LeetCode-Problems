9857037a6fce8416a655e12b9b5d1addeef46004
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-07-11 00:09:07 +0300

    using another more efficient approach to solve two sum

--- Abo El-Seoud/Arrays & Hashing/Two Sum.js
+++ Abo El-Seoud/Arrays & Hashing/Two Sum.js
@@ -1,3 +1,4 @@
+//------------------------------------------------------First Approach
 var twoSum = function (array, target) {
   let map = new Map();
   for (let i = 0; i < array.length; i++) {
@@ -10,6 +11,26 @@ var twoSum = function (array, target) {
   }
 };
 
+//------------------------------------------------------Second Approach
+var twoSum = function (array, target) {
+  let map = new Map();
+  for (let i = 0; i < array.length; i++) {
+    let complement = target - array[i];
+    if (map.has(array[i])) {
+      return [map.get(array[i]), i];
+    } else {
+      map.set(complement, i);
+    }
+  }
+};
+
+//-------------//-------------//-------------Test Cases--------------//--------------//--------------//
 console.log(twoSum([2, 7, 11, 15], 9));
 console.log(twoSum([3, 2, 4], 6));
 console.log(twoSum([3, 3], 6));
+
+//-------------//-------------//-------------concolusion--------------//--------------//--------------//
+
+// The second approach, which involves storing the complement along with the index of the corresponding element, is superior to storing the element itself with its index and then checking if the complement required is one of the stored elements.
+
+// So the second approach is more efficient because it consumes less memory and requires almost less time for execution.
