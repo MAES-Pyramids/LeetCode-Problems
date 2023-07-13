712202cac644e5b4cecbc3a9948b202507d824f2
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2023-07-13 23:28:37 +0300

    optimize TopKFrequent
    solved in O(n) in time complexity

--- Salman/Arrays & Hashing/TopKFrequent.kt
+++ Salman/Arrays & Hashing/TopKFrequent.kt
@@ -1,9 +1,40 @@
-fun topKFrequent(nums: IntArray, k: Int): IntArray {
-      val map = mutableMapOf<Int, Int>()
-      for (num in nums) {
-          map[num] = map.getOrDefault(num, 0) + 1
-      }
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val frequency = mutableMapOf<Int, List<Int>>() // key = frequency, value = nums with the frequency
+        val numCount = mutableMapOf<Int, Int>() //  key = num, value = frequency
 
-      val valuesSorted = map.entries.sortedBy { it.value }
-      return valuesSorted.takeLast(k).map { it.key }.toIntArray()
-  }
+        nums.forEach {
+            numCount[it] = numCount.getOrDefault(it, 0) + 1
+        }
+
+        numCount.forEach { (t, u) ->
+            frequency[u] = frequency.getOrDefault(u, emptyList()) + t
+        }
+
+        var maxFrequency = frequency.getMaxFrequency().first
+        val result = hashSetOf<Int>()
+
+        while (maxFrequency >= 0) {
+            if (result.size >= k) {
+                break
+            }
+            frequency[maxFrequency]?.let { result.addAll(it) }
+            maxFrequency--
+        }
+
+
+
+        return result.toIntArray()
+    }
+
+    fun Map<Int, List<Int>>.getMaxFrequency(): Pair<Int, List<Int>> {
+        var max = Int.MIN_VALUE
+        var key: Int? = null
+        forEach { (t, u) ->
+            if (t > max) {
+                key = t
+                max = t
+            }
+        }
+        val res = if (key == null) 0 to this[0]!! else key!! to this[key!!]!!
+        return res
+    }
