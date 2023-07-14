1c6e6449cfe71194a7a4a6020b6a4789574e11c9
Muhammed Salman <84887514+muhammed9865@users.noreply.github.com>
2023-07-14 21:44:18 +0300

    Update GroupAnagrams.kt
    Re solve in n * m lg m, Fuck you abo elsoud

--- Salman/Arrays & Hashing/GroupAnagrams.kt
+++ Salman/Arrays & Hashing/GroupAnagrams.kt
@@ -2,11 +2,11 @@ fun groupAnagrams(strs: Array<String>): List<List<String>> {
       if (strs.size == 1) {
           listOf(listOf(strs[0]))
       }
-      val sortedStrs = strs.map { it.sort() } // n^2 lgn
-      val list = mutableMapOf<String, List<String>>()
+      val sortedStrs = strs.map { it.sort() } // n * m lgm
+      val map = mutableMapOf<String, List<String>>()
 
       for (i in sortedStrs.indices) {
-          list[sortedStrs[i]] = list.getOrDefault(sortedStrs[i], emptyList()) + strs[i]
+          map[sortedStrs[i]] = map.getOrDefault(sortedStrs[i], emptyList()) + strs[i]
       }
 
       return list.map { it.value }
