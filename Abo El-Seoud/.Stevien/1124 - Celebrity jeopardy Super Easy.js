65cb2543702c1a0566a8a8260d589cdcbc7362d2
MAES-Pyramids <mohamedaboelseoud@std.mans.edu.eg>
2023-07-19 16:29:04 +0300

    1124 - Celebrity jeopardy

--- Abo El-Seoud/.Stevien/1124 - Celebrity jeopardy Super Easy.js
+++ Abo El-Seoud/.Stevien/1124 - Celebrity jeopardy Super Easy.js
@@ -1,37 +1,9 @@
 //------------------------------------------------------------------------- First Approach
-let TEX = function (input) {
-  let start = true;
-  input = input.split("");
-  for (let char in input) {
-    if (input[char] == '"' && start) {
-      input.splice(char, 1, "``");
-      start = false;
-    } else if (input[char] == '"' && !start) {
-      input.splice(char, 1, "''");
-      start = true;
-    }
-  }
-  console.log(input.join(""));
+let Celebrity = function (input) {
+  return input;
 };
 //------------------------------------------------------------------------- Second Approach
-// let TEX = function (input) {
-//   let start = true;
-//   input = input.split("");
-//   for (let char in input) {
-//     if (input[char] == '"' && start) {
-//       process.stdout.write("``");
-//       start = false;
-//     } else if (input[char] == '"' && !start) {
-//       process.stdout.write("''");
-//       start = true;
-//     } else {
-//       process.stdout.write(input[char]);
-//     }
-//   }
-// };
+
 //------------------------------------------------------------------------- Test Cases
-TEX(`"To be or not to be," quoth the Bard, "that
-is the question"`);
-TEX(
-  `The programming contestant replied: "I must disagree.To \`C' or not to \`C', that is The Question!"`
-);
+console.log(Celebrity("Y = 3"));
+console.log(Celebrity("X=9"));
